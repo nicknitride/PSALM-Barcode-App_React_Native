6b45610e00a5e7e6ef4655fb37fd491f54d2a83a
Nicholo Pardines <nick123abl@gmail.com>
2024-07-01 16:59:42 +0800

    Current state

--- app/(tabs)/TestParsePage.tsx
+++ app/(tabs)/TestParsePage.tsx
@@ -5,10 +5,12 @@ import * as FileSystem from 'expo-file-system';
 import * as DocumentPicker from 'expo-document-picker';
 import Papa from 'papaparse';
 import * as Sharing from 'expo-sharing';
+import {router} from 'expo-router';
 
 export default function TestParsePage(){
     const [db, setDb] = useState(SQLite.openDatabaseAsync('example.db'));
-    const [globalFileContent, SetGlobalFileContent] = useState()
+    const [globalFileContent, SetGlobalFileContent] = useState();
+    const [fileLocation, setFileLocation] = useState();
     const [docReqResult, setDocRequest] = useState<DocumentPicker.DocumentPickerResult>()
 
     const exportDb = async () => {
@@ -43,13 +45,16 @@ export default function TestParsePage(){
             console.log(docRequest.output)
             console.log(docRequest.assets[0].uri)
             if (docRequest.assets[0].uri !== null){
+                setFileLocation(docRequest.assets[0].uri)
+                console.log("File Location: "+fileLocation)
                 const fileContent = await FileSystem.readAsStringAsync(docRequest.assets[0].uri)
-                console.log("File Content: ",fileContent)
+                // console.log("File Content: ",fileContent)
                 const parsedData = Papa.parse(fileContent,{
                     header:true,
                     transformHeader: (header:any) => header.trim(), // trim trailing spaces from header
                     skipEmptyLines: true,
-                    complete:(results:any)=>{console.log("Parsed Data: ", results.data[0])
+                    complete:(results:any)=>{
+                        console.log("Parsed Data: ", results)
                         SetGlobalFileContent(results.data)
                     }, //Use  a for each to insert into the sqlite db
                     error:()=>{console.log("CSV Could not be read")}
@@ -62,16 +67,18 @@ export default function TestParsePage(){
         catch(error){
             console.log(error)
         }
-        const base64 = await FileSystem.readAsStringAsync(
-            result.uri,
-            {
-              encoding: FileSystem.EncodingType.Base64
-            }
-          );
-    
-          await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'SQLite/example.db', base64, { encoding: FileSystem.EncodingType.Base64 });
-          (await db).closeAsync
-          setDb(SQLite.openDatabaseAsync('example.db'));
+        // const base64 = await FileSystem.readAsStringAsync(
+        //     String(fileLocation),
+        //     {
+        //       encoding: FileSystem.EncodingType.Base64
+        //     }
+        //   );
+        // Replace with an actual string composed of papaparse data
+
+        // Uncomment Below later on
+        //   await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'SQLite/example.db', base64, { encoding: FileSystem.EncodingType.Base64 });
+        //   (await db).closeAsync
+        //   setDb(SQLite.openDatabaseAsync('example.db'));
     }
     return(
         <View>
@@ -80,6 +87,9 @@ export default function TestParsePage(){
                 console.log("Import clicked")
                 }}></Button>
             <Button title='Export CSV' onPress={()=>{exportDb()}} />
+            <Button title = 'Open Database Creation/Test Page' onPress={()=>{
+                router.push("/testing/dbtestpage")
+            }}/>
             <ScrollView>
             <Text>Parsed JSON: {JSON.stringify(docReqResult?.assets)}</Text>
             <Text>Parsed Data: {JSON.stringify(globalFileContent)}</Text>
---
+++ app/testing/dbtestpage.tsx
@@ -0,0 +1,86 @@
+import {View, Text} from 'react-native';
+import { Stack } from 'expo-router';
+import { useState, useEffect } from 'react';
+import * as SQLite from 'expo-sqlite';
+import Button from '../styled-components/Button';
+
+export default function dbtestpage(){
+    const db = SQLite.openDatabaseSync('test.db');
+    const initDb = async () =>{
+        console.log(db)
+        await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS item (Article_Item TEXT,
+        Description TEXT,
+        Old_Property_Number INTEGER,
+        New_Property_Number INTEGER PRIMARY KEY,
+        Unit_of_Measure TEXT,
+        Unit_Value TEXT,
+        Quantity_per_Property_Card TEXT,
+        Quantity_per_Physical_Count INTEGER,
+        Location_Whereabouts TEXT,
+        Condition TEXT,
+        Remarks TEXT
+        );
+       `)
+    }
+    initDb()
+    const [display, setDisplay] = useState<any>();
+
+
+    const addInitial = async () => {
+        try {
+            await db.execAsync(`
+                INSERT INTO item (
+                    Article_Item,
+                    Description,
+                    Old_Property_Number,
+                    New_Property_Number,
+                    Unit_of_Measure,
+                    Unit_Value,
+                    Quantity_per_Property_Card,
+                    Quantity_per_Physical_Count,
+                    Location_Whereabouts,
+                    Condition,
+                    Remarks
+                )
+                VALUES (
+                    "Test Item",
+                    "Description here",
+                    1234,
+                    5678,
+                    "km",
+                    "3000",
+                    "4",
+                    "None",
+                    "In the backrooms",
+                    "In poor condition",
+                    "Need to replace soon"
+                );
+            `);
+            console.log("Insertion successful");
+        } catch (error) {
+            setDisplay(JSON.stringify(error))
+        }
+    }
+    // addInitial()
+
+    const showData = () =>{
+        const allData = db.getAllSync(`SELECT * FROM item`)
+        console.log(allData)
+        setDisplay(JSON.stringify(allData))
+    }
+
+    return(<><View>
+        <Stack.Screen options={{headerTitle:"DB Testing Page"}}/>
+        <Button title="Get Items" onPress={()=>{
+            showData()}}></Button>
+        <Text>{display}</Text>
+        </View></>)
+}
+
+/* 
+Headers:
+Article_Item, Description,Old_Property_Number,New_Property_Number,Unit_of_Measure,Unit_Value,Quantity_per_Property_Card,
+Quantity_per_Physical_Count,Location_Whereabouts,Condition,Remarks
+
+*/
