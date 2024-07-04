b4f317927efa56684eeb310f2be1325a13f3abf0
Nicholo Pardines <nick123abl@gmail.com>
2024-07-04 09:11:09 +0800

    Added a types file

--- app/itemview/[id].tsx
+++ app/itemview/[id].tsx
@@ -1,33 +1,38 @@
-import {View,Text} from 'react-native'
+import {View,Text, ScrollView} from 'react-native'
 import Button from '../styled-components/Button'
 import {Stack, useLocalSearchParams} from 'expo-router'
+import * as dbfunc from '../DatabaseFunctions';
+import * as SQLite from 'expo-sqlite'
+import {dbEntry} from '../types'
+import { router } from 'expo-router';
 // https://stackoverflow.com/questions/78246345/react-native-usesearchparams-hook-from-expo-router
+// SQLITE Expo tutorial: https://github.com/chelseafarley/expo-sqlite-tutorial/blob/main/App.js
+
+
+
 
 export default function itemEditView(){
+    const db = dbfunc.startDb();
+    dbfunc.initDb();
+
     const {id} = useLocalSearchParams();
-    // SQLITE Expo tutorial: https://github.com/chelseafarley/expo-sqlite-tutorial/blob/main/App.js
+    const correspondingRow = db.getFirstSync<dbEntry>(`SELECT * FROM item WHERE New_Property_Number = ${id}`);
     return(
         <>
         <View>
             <Stack.Screen options={{headerTitle:"Edit View"}}/>
-            <Text style={{fontWeight:'bold'}}>Editing Item: {id}</Text>
-            {/* TODO: ONCE BACKEND IS FINISHED ADD SQLITE CODE TO PULL DATA AND TURN THIS INTO A FORM THAT EDITS IT */}
-            {/* THEN PUSH THE EDITED ITEM TO THE TABLE DISPLAYED IN THE INVENTORY LIST */}
-            {/* SQLITE Implementation Idea:
-                1. Papaparse - Parse the CSV generated for the singular table and convert to JSON
-                2. Write a command that creates the tables if it doesn't exist given the inventory structure
-                    2.a Recents table - this table is what you display in the inventory tab (only display rows with changed values)
-                    2.b General table - this table holds the imported data 
-                    Install command: npx install papaparse
-                    Github for Expo SQLite YT Tutorial: https://github.com/chelseafarley/expo-sqlite-tutorial/blob/main/App.js
-                    YT Video: https://www.youtube.com/watch?v=1kSLd9oQX7c&t=90s
-                    The YT Video includes the import function and export function
-                3. Write a custom function that takes the exported db and converts it back into a CSV or another usable format
-            */}
-            {/* General Implementation Idea:
-                1. Inventory requires a function to display the recently edited table in reverse chronological order
-                2. A clickable card that reopens an 'itemview/[id]' that allows editing 
-            */}
+            <Text style={{fontWeight:'bold',fontSize:20}}>Editing Item: {id}</Text>
+            <ScrollView>
+                {(correspondingRow !==null) && <>
+                <View>
+                    <Text>Article Item: {correspondingRow.Article_Item}</Text>
+                    <Text>Description: {correspondingRow.Description}</Text>
+                    <Text>Condition: {correspondingRow.Condition}</Text>
+                    <Text>Remarks: {correspondingRow.Remarks}</Text>
+                </View>
+                </>}
+            </ScrollView>
+            <Button title="Cancel" onPress={router.navigate('/Home')}></Button>
         </View>
         </>
     );
---
+++ app/types.ts
@@ -0,0 +1,13 @@
+export type dbEntry = {
+    Article_Item: String;
+    Description:String;
+    Old_Property_Number:number;
+    New_Property_Number:number;
+    Unit_of_Measure:String;
+    Unit_Value:String;
+    Quantity_per_Property_Card:String;
+    Quantity_per_Physical_Count:String;
+    Location_Whereabouts:String;
+    Condition:String;
+    Remarks:String;
+}
