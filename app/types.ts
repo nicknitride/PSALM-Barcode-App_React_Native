6bb77e7ad41eb552b259a0cf9df36cc0fcbf121b
Nicholo Pardines <nick123abl@gmail.com>
2024-07-05 10:16:12 +0800

    Fixed types.ts

--- app/types.ts
+++ app/types.ts
@@ -1,13 +1,13 @@
 export type dbEntry = {
-    Article_Item: String;
-    Description:String;
-    Old_Property_Number:number;
-    New_Property_Number:number;
-    Unit_of_Measure:String;
-    Unit_Value:String;
-    Quantity_per_Property_Card:String;
-    Quantity_per_Physical_Count:String;
-    Location_Whereabouts:String;
-    Condition:String;
-    Remarks:String;
+    Article_Item: string;
+    Description:string;
+    Old_Property_Number:string;
+    New_Property_Number:string;
+    Unit_of_Measure:string;
+    Unit_Value:string;
+    Quantity_per_Property_Card:string;
+    Quantity_per_Physical_Count:string;
+    Location_Whereabouts:string;
+    Condition:string;
+    Remarks:string;
 }
