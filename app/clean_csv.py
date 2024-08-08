6ad316672df8a070acf13e7da39b1ed8a06182ff
is-this-right <130638799+is-this-right@users.noreply.github.com>
2024-08-08 10:14:53 +0800

    Revised clean_csv according to requirements

--- app/clean_csv.py
+++ app/clean_csv.py
@@ -1,31 +1,24 @@
 import pandas as pd
+import numpy as np
 
-csv_file ='Master-List-of-PPEs-for-CBK-HEPPs.xlsx - Botocan.csv' # put csv file here
-"""
-csv_file must either be:
-* a path to a file (a str, pathlib.Path, or py:py._path.local.LocalPath)
-* URL (including http, ftp, and S3 locations)
-* any object with a read() method (such as an open file or StringIO).
-"""
 
-# creates DataFrame object, ignoring PSALM template header and footer
-df = pd.read_csv(csv_file, skiprows = 8, skipfooter=10)
+# rename fields
+# {field_in_excel: program_compatible_field_name}
+df = pd.read_csv('Master-List-of-PPEs-for-CBK-HEPPs.xlsx - Botocan.csv', skiprows = 8, skipfooter=10)
 db_fields = {'Article / Item': 'Article_Item',
             'Old Property No. assigned': 'Old_Property_Number',
              'New Property No. assigned (To be filled up during validation)': 'New_Property_Number',
              'Unit of Measure': 'Unit_of_Measure',
-             'Unit Value': 'Unit_Value',
+             ' Unit  Value': 'Unit_Value',
              'Quantity per Property Card': 'Quantity_per_Property_Card',
              'Quantity per Physical Count': 'Quantity_per_Physical_Count',
              'Location / Whereabouts': 'Location_Whereabouts',
-            }
+            }    
 df.rename(columns= db_fields, inplace=True)
-last = ""
-def fill_empty_property_number(row):
-    if row['Article_Item'] == 'MAIN POWER HOUSE':
-        return last
-    else:
-        last = row['New_Property_Number']
-        return last  # Otherwise, return the current value of column 'A'
-df['New_Property_Number'] = df.apply(fill_empty_property_number, axis=1)
-df.to_csv(csv_file, index=False)
+
+# ignore all without property numbers
+df = df.dropna(subset=['New_Property_Number'])
+df = df.fillna("")
+
+file_name = input("Name the exported file: ")
+df.to_csv(file_name)
